d03f85f6ccd3bb95fe46290b90214a79efa925bb
Dostoevskaya Anna <iwantknow.aboutjt68h43@gmail.com>
2022-02-08 00:28:36 +0600

    Refactoring. * routes.py, get_caps. Perform code refactoring * routes.py, get_brand. Perform code refactoring * Example. auxil_func/get_caps.py, calc_pointer_id_in_db. #documentation *  config.py, base_url_generate.

--- api/routes.py
+++ api/routes.py
@@ -1,72 +1,63 @@
 import os
 
-from api import app, conf, DBSession
-from api.models import Cap, CapsBrand
+from api import app, conf
+from api.models import CapsBrand
 from fastapi.responses import FileResponse
 
-def calc_pointer_id(page: int, pg_size: int):
-    return ((page - 1) * pg_size) + 1
+from api.auxil_func.get_caps import *
+from api.auxil_func.get_brand import *
 
 @app.get('/')
 async def root():
     return {'name_api': 'CapsApi'}
 
 @app.get('/api/v1/caps/')
-async def get_caps(page=1, pg_size=5):
-    ## TODO(annad): Perform code refactoring
+async def get_caps(page: int = 1, pg_size: int = 5):
+    if (page <= 0) or (pg_size <= 0):
+        return None
 
-    page = int(page)
-    pg_size = int(pg_size)
-    res = {}
+    pointer_id = calc_pointer_id_in_db(page, pg_size) ## NOTE(annad): How can this be done correctly?
+    caps = get_caps_db_request(pointer_id, pg_size)
 
-    ## TODO(annad): How can this be done correctly?
-    pointer_id = calc_pointer_id(page, pg_size)
+    if len(caps) == 0:
+        return None
 
-    with DBSession() as sess:
-        caps = sess.query(Cap).filter(Cap.id >= pointer_id, Cap.id < pointer_id + pg_size).all()
+    res = {
+        'count': '',
+        'next': '',
+        'previous': '',
+        'results': []
+    }
 
     res['count'] = len(caps)
+    res['next'] = None if res['count'] < pg_size else conf.base_url_generate('/api/' + conf.API_VER + '/caps/') + \
+                                                      '?page={page}&pg_size={pg_size}'.format(page=page+1, pg_size=pg_size)
+    res['previous'] = None if page == 1 else conf.base_url_generate('/api/' + conf.API_VER + '/caps/') + \
+                                             '?page={page}&pg_size={pg_size}'.format(page=page-1, pg_size=pg_size)
 
-    if res['count'] < pg_size:
-        res['next'] = None
-    else:
-        res['next'] = 'http://192.168.2.136:8000' + '/api/v1/caps/' + \
-                      '?page={page}&pg_size={pg_size}'.format(page=page+1, pg_size=pg_size)
-
-    if page == 1:
-        res['previous'] = None
-    else:
-        res['previous'] = 'http://192.168.2.136:8000' + '/api/v1/caps/' + \
-                          '?page={page}&pg_size={pg_size}'.format(page=page-1, pg_size=pg_size)
-
-    res['results'] = []
     for cap in caps:
-        cap.image = 'http://192.168.2.136:8000/' + cap.image
+        cap.image = conf.base_url_generate('/' + cap.image)
         res['results'].append(cap.get_dict_repr())
 
     return res
 
-@app.get('/api/v1/brands/{brand_id}')
-async def get_brand(brand_id):
-    ## TODO(annad): Perform code refactoring
-    brand_id = int(brand_id)
 
-    with DBSession() as sess:
-        brand = sess.query(CapsBrand).filter(CapsBrand.id == brand_id).all()
+@app.get('/api/v1/brands/{brand_id}')
+async def get_brand(brand_id: int = 1):
+    if brand_id <= 0:
+        return None
+
+    brand = get_caps_brand_db_request(brand_id)
 
     if len(brand) != 1:
         return None
 
     brand: CapsBrand = brand[0]
     res = brand.get_dict_repr()
-    res['image'] = 'http://192.168.2.136:8000/' + res['image']
+    res['image'] = conf.base_url_generate('/' + res['image'])
 
     return res
 
-'''
-LINK FOR TEST: 
-http://192.168.2.136:8000/media/caps/20220206172612100.jpg
-'''
 @app.get('/media/{directory}/{name}')
 async def get_media_cap(directory, name):
     image_path = os.path.join(directory, name)
--- config.py
+++ config.py
@@ -5,9 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     IP_ADDRESS = os.environ.get('IP_ADDRESS') or '192.168.2.136'
     PORT = os.environ.get('PORT_CAPS_API') or '8000'
-
     STATIC_IMAGE_DIR = 'media/'
     SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
+    API_VER = 'v1'
+
+    def base_url_generate(self, body=''):
+        return 'http://' + self.IP_ADDRESS + ':' + self.PORT + body
