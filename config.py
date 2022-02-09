412c80686c73b99725403f213471f0793deec2b4
Dostoevskaya Anna <iwantknow.aboutjt68h43@gmail.com>
2022-02-10 00:09:17 +0600

    config.py, API_VER. API_VER get from os.environ

--- config.py
+++ config.py
@@ -6,11 +6,12 @@ class Config:
     IP_ADDRESS = os.environ.get('IP_ADDRESS') or '192.168.2.136'
     PORT = os.environ.get('PORT_CAPS_API') or '8000'
     STATIC_IMAGE_DIR = 'media/'
+    API_VER = os.environ.get('CAPS_API_VER') or 'v1'
+
     SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    API_VER = 'v1'
 
     def base_url_generate(self, body=''):
         return 'http://' + self.IP_ADDRESS + ':' + self.PORT + body
