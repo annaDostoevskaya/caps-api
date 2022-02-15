3722a3607e146d0b497bb58d53583d14d587624f
Dostoevskaya Anna <iwantknow.aboutjt68h43@gmail.com>
2022-02-16 01:12:33 +0600

    NOTE about postgre:// and postgresql:// problem. SQLAlchemy interprets just postgresql:// in code. But ENV_VAR in heroku created with posrgre:// prefix. You must know how it's change for normal work of service.

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     IP_ADDRESS = os.environ.get('IP_ADDRESS') or '192.168.2.136'
     PORT = os.environ.get('PORT_CAPS_API') or '8000'
+
+    ## NOTE(annad):
+    ## If you get a postgres module loading error like this:
+    ## https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
+    ## This means that you most likely need to change the prefix in the URL to the database
+    ## In the heroku plugin, this cannot be done just like that, so you need to follow this set of commands:
+    ## https://stackoverflow.com/questions/35061914/how-to-change-database-url-for-a-heroku-application
     APPLICATION_URL = os.environ.get('APPLICATION_URL') or 'http://' + IP_ADDRESS + ':' + PORT
 
     STATIC_IMAGE_DIR = 'media/'
@@ -15,5 +22,5 @@ class Config:
     SQLALCHEMY_ECHO = True
 
 
-    def base_url_generate(self, body=''):
+    def base_url_generate(self, body='/'):
         return self.APPLICATION_URL + body
