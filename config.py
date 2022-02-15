56de82a504690d20650ca764b35abeb0bbb4ebbe
Dostoevskaya Anna <iwantknow.aboutjt68h43@gmail.com>
2022-02-15 22:39:19 +0600

    config.py, Refactoring.

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     IP_ADDRESS = os.environ.get('IP_ADDRESS') or '192.168.2.136'
     PORT = os.environ.get('PORT_CAPS_API') or '8000'
-
-    DOMEN_L3 = os.environ.get('DOMEN_L3') or 'caps-api'
-    HOST = os.environ.get('NAME_OF_HOSTING') or 'herokuapp.com'
+    APPLICATION_URL = os.environ.get('APPLICATION_URL') or IP_ADDRESS + ':' + PORT
 
     STATIC_IMAGE_DIR = 'media/'
     API_VER = os.environ.get('CAPS_API_VER') or 'v1'
@@ -17,16 +15,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
-    ## TODO(annad): Refactoring.
-    ON_HOSTING = os.environ.get('WE_ARE_ON_HOSTING') or False
-    if ON_HOSTING:
-        ON_HOSTING = int(ON_HOSTING)
-
-    APPLICATION_URL = ''
-    if ON_HOSTING:
-        APPLICATION_URL = 'https://' + DOMEN_L3 + '.' + HOST
-    else:
-        APPLICATION_URL = 'http://' + IP_ADDRESS + ':' + PORT
 
     def base_url_generate(self, body=''):
         return self.APPLICATION_URL + body
